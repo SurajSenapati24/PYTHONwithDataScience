16c293aefe66ea3907dfa0a76beb49c865010bc1
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-01 19:52:02 +0530

    update

---
+++ MinorAssignment_3/Q4.py
@@ -0,0 +1,6 @@
+#Write a function that takes a string as input and 
+#returns the reversed string.
+def reverseString(s):
+    return s[::-1]
+s="Suraj"
+print(reverseString(s))
---
+++ MinorAssignment_3/Q6.py
@@ -0,0 +1,9 @@
+#Define a function to check if a given string is a palindrome. Example: madam ⟲ madam, racecar ⟲ racecar.
+def palindrome(s):
+    n=s[::-1]
+    if s==n:
+        return True
+    else:
+        False
+user=input("Enter a String: ")
+print(palindrome(user))
