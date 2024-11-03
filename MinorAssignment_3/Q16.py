bf9749132c543a375e4d313f302c043f8aad4b8e
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-03 16:22:05 +0530

    update

--- MinorAssignment_3/Q16.py
+++ MinorAssignment_3/Q16.py
@@ -1,25 +1,20 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
-    
-user=int(input("Enter choice: (1 for additon, 2 for subtraction, 3 for multiplication, 4 for division): "))
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-if user==1:
-    print("Addition is: ",add(a,b))
-elif user==2:
-    print("Subtraction is: ", sub(a,b))
-elif user==3:
-    print("Multiplication is: ", mul(a,b))
-elif user==4:
-    if b==0:
-        print("Division can't be performed as b=0")
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error: Division by zero is undefined."
     else:
-        print(f"Division is: {div(a,b):.2f}")
-else:
-    print("Invalid choice!!")
+        return "Error: Invalid operator."
+
+    return f"The result is: {result}"
+print(calculator())
