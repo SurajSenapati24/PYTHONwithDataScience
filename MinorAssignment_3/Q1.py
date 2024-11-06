75fcea748db49d08d31209a50538fde9fe595f7d
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-06 20:51:09 +0530

    upated Q1

--- MinorAssignment_3/Q1.py
+++ MinorAssignment_3/Q1.py
@@ -1,12 +1,12 @@
-def find_greatest_digits(number):
-    num_str = str(number)
+def find_greatest_digits(num):
+    num_str = str(num)
     digits = [int(char) for char in num_str]
-    unique_digits = list(set(digits))
-    unique_digits.sort(reverse=True)
-    first = unique_digits[0] if len(unique_digits) > 0 else None
-    second = unique_digits[1] if len(unique_digits) > 1 else None
-    third = unique_digits[2] if len(unique_digits) > 2 else None
-    print(f"{first} {second} {third}")
+    unique = list(set(digits))
+    unique.sort(reverse=True)
+    first = unique[0] if len(unique) > 0 else None
+    second = unique[1] if len(unique) > 1 else None
+    third = unique[2] if len(unique) > 2 else None
+    print(f"{first},{second},{third}")
 
-number = 123
-find_greatest_digits(number)
+num = 123
+find_greatest_digits(num)
