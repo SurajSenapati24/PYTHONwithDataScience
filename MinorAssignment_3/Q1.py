6e0888bccdbe8b99afd51e688962f80246c17951
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-06 20:59:51 +0530

    update

--- MinorAssignment_3/Q1.py
+++ MinorAssignment_3/Q1.py
@@ -6,7 +6,7 @@ def find_greatest_digits(num):
     first = unique[0] if len(unique) > 0 else None
     second = unique[1] if len(unique) > 1 else None
     third = unique[2] if len(unique) > 2 else None
-    print(f"{first},{second},{third}")
+    print(f"{first},{second},{third}")  
 
 num = 123
 find_greatest_digits(num)
