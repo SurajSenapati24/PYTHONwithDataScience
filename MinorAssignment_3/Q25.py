1da835dde956afea5f59c25baffc20212d6eabac
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-09 00:12:11 +0530

    updated

--- MinorAssignment_3/Q25.py
+++ MinorAssignment_3/Q25.py
@@ -3,12 +3,12 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
-def are_coprime(num1, num2):
-    return gcd(num1, num2) == 1
+def are_coprime(n1, n2):
+    return gcd(n1, n2) == 1
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-if are_coprime(num1, num2):
-    print(f"{num1} and {num2} are coprime.")
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+if are_coprime(n1, n2):
+    print(f"{n1} and {n2} are coprime.")
 else:
-    print(f"{num1} and {num2} are not coprime.")
+    print(f"{n1} and {n2} are not coprime.")
