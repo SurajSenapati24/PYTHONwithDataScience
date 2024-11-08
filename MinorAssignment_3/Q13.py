24c7fa0b6c66d67bb111ee0b8933f53b86e2fb4c
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-08 21:10:42 +0530

    update

--- MinorAssignment_3/Q13.py
+++ MinorAssignment_3/Q13.py
@@ -1,14 +1,14 @@
 def roman_to_integer(roman):
-    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    prev_value = 0
+    prev_v = 0
     for char in reversed(roman):
-        current_value = roman_values[char]
-        if current_value < prev_value:
-            total -= current_value
+        curr_v = romans[char]
+        if curr_v < prev_v:
+            total -= curr_v
         else:
-            total += current_value
-        prev_value = current_value
+            total += curr_v
+        prev_v = curr_v
     return total
 
 print(roman_to_integer("LVIII"))
