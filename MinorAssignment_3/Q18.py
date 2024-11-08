3f678270bbc12ef7dd7ac4d2e13657f8119542ec
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-08 21:34:18 +0530

    update

--- MinorAssignment_3/Q18.py
+++ MinorAssignment_3/Q18.py
@@ -1,11 +1,11 @@
-def is_anagram(str1, str2):
-    str1 = str1.replace(" ", "").lower()
-    str2 = str2.replace(" ", "").lower()
-    return sorted(str1) == sorted(str2)
+def is_anagram(s1, s2):
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+    return sorted(s1) == sorted(s2)
 
-string1 = input("Enter the first string: ")
-string2 = input("Enter the second string: ")
-if is_anagram(string1, string2):
+s1 = input("Enter the first string: ")
+s2 = input("Enter the second string: ")
+if is_anagram(s1, s2):
     print("The strings are anagrams.")
 else:
     print("The strings are not anagrams.")
