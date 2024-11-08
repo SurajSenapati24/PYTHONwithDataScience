6ac832ee1c32be68413ee99375aa3bb0aaab5903
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-08 21:45:10 +0530

    update

--- MinorAssignment_3/Q22.py
+++ MinorAssignment_3/Q22.py
@@ -1,10 +1,10 @@
-def print_ap_terms(f_term, difference):
+def print_ap_terms(f_term, diff):
     terms = []
     for i in range(10):
-        term = f_term + i * difference
+        term = f_term + i * diff
         terms.append(term)
     print("The first 10 terms of the arithmetic progression are:", terms)
 
 f_term = int(input("Enter the first term of the AP: "))
-difference = int(input("Enter the common difference of the AP: "))
-print_ap_terms(f_term, difference)
+diff = int(input("Enter the common difference of the AP: "))
+print_ap_terms(f_term, diff)
