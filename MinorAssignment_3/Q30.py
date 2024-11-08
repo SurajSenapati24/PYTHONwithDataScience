eb0ec7d51a01205afa9fc3f4cdfdc048c8c1eaef
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-09 01:15:48 +0530

    update

--- MinorAssignment_3/Q30.py
+++ MinorAssignment_3/Q30.py
@@ -1,4 +1,4 @@
-def product_of_digits(num):
+def prod_of_digits(num):
     prod=1
     while num>0:
         digit=num%10
@@ -6,4 +6,4 @@ def product_of_digits(num):
         num//=10
     return prod
 user=int(input("Enter a number: "))
-print("The product of its digit is: ",product_of_digits(user))
+print("The product of its digit is: ",prod_of_digits(user))
