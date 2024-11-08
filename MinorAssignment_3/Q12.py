f2a0a44522d5f5976ab4aa3199064a98740d5814
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-08 21:07:18 +0530

    update

--- MinorAssignment_3/Q12.py
+++ MinorAssignment_3/Q12.py
@@ -4,14 +4,14 @@ def get_day_of_week(date):
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     return days[date.weekday()]
 
-def calculate_new_date_and_day(today_date, today_day, days_to_add):
-    today_date = datetime.strptime(today_date, "%Y-%m-%d")
-    new_date = today_date + timedelta(days=days_to_add)
-    new_day = get_day_of_week(new_date)
-    return new_date.strftime("%Y-%m-%d"), new_day
-today_date = input("Today's date (YYYY-MM-DD): ")
-today_day = input("Today's day: ")
+def cal_n_date_and_day(t_date, t_day, days_to_add):
+    t_date = datetime.strptime(t_date, "%Y-%m-%d")
+    n_date = t_date + timedelta(days=days_to_add)
+    n_day = get_day_of_week(n_date)
+    return n_date.strftime("%Y-%m-%d"), n_day
+t_date = input("Today's date (YYYY-MM-DD): ")
+t_day = input("Today's day: ")
 days_to_add = int(input("Number of days: "))
-new_date, new_day = calculate_new_date_and_day(today_date, today_day, days_to_add)
-print("New date:", new_date)
-print("New day:", new_day)
+n_date, n_day = cal_n_date_and_day(t_date, t_day, days_to_add)
+print("New date:", n_date)
+print("New day:", n_day)
