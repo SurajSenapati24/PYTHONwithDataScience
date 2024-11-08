a2f7bc612b5c78a19cb10df73337f26906b8c2f1
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-08 20:44:43 +0530

    update

--- MinorAssignment_3/Q10.py
+++ MinorAssignment_3/Q10.py
@@ -1,7 +1,7 @@
-from itertools import permutations
+from itertools import permutations as p
 
 def unique_permutations(s):
-    unique_perms = set(permutations(s))
+    unique_perms = set(p(s))
     return [''.join(p) for p in unique_perms]
 user = input("Enter a string: ")
 print(unique_permutations(user))
