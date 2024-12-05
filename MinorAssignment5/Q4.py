eea6ff0a9d361581e1c8e0271cb25414271be0b0
Suraj Senapati <surajsenapati58@gmail.com>
2024-12-05 22:55:46 +0530

    Q4 and Q5 solution is added

---
+++ MinorAssignment5/Q4.py
@@ -0,0 +1,5 @@
+"""Make an English-to-French dictionary called e2f and print it. Here are your starter words: dog is
+ chien, cat is chat, and walrus is morse."""
+e2f={'dog':'chien','cat':'chat','walrus':'morse'}
+print("English to French Dictionary: ")
+print(e2f)
---
+++ MinorAssignment5/Q5.py
@@ -0,0 +1,17 @@
+tlds = {'Canada': 'ca', 'United States': 'us', 'Mexico': 'mx'}
+#a) Check whether the dictionary contains the key ‘Canada’.
+print(f"Contains 'Canada':{'Canada' in tlds}")
+# b) Check whether the dictionary contains the key ‘France’.
+print(f"Contains 'France':{'France' in tlds}")
+# c) Iterate through the key-value pairs and display them in a two-column format.
+print("\nCountry and TLD (Two-Column Format):")
+for c, t in tlds.items():
+    print(f"{c:15} {t}")
+#d) Add the key–value pair ‘Sweden’ and ‘sw’ (incorrect TLD).
+tlds['Sweden']='sw'
+print(tlds)
+# e) Update the value for the key ‘Sweden’ to ‘se’ (correct TLD).
+tlds['Sweden']='se'
+print(tlds)
+tlds_rev={val:key for key,val in tlds.items()}
+print(tlds_rev)
