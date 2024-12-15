191be65bfe406f63913af963ae256ec1d2f229dd
Suraj Senapati <surajsenapati58@gmail.com>
2024-12-15 21:33:20 +0530

    update

--- MinorAssignment6/Q6.py
+++ MinorAssignment6/Q6.py
@@ -1,8 +1,13 @@
 import numpy as np
 a=2**np.arange(6)
 a=a.reshape(2,3)
+print("Original Array before flatten/ravel: ")
 print(a)
 a_f=a.flatten()
+a_f[2]=34
 print("Flattened a: ",a_f)
 a_r=a.ravel()
+a_r[2]=44
 print("Ravelled a: ",a_r)
+print("Original Array after flatten/ravel: ")
+print(a)
