8e6b9d4f9d2cf51fc61dbeedb4c14db6e670bcdc
Suraj Senapati <surajsenapati58@gmail.com>
2025-03-19 23:58:38 +0530

    Update

---
+++ 6thSemester/MinorAssignment2/Q11.py
@@ -0,0 +1,22 @@
+def merge_sorted_lists(list1, list2):
+    result = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            result.append(list1[i])
+            i += 1
+        else:
+            result.append(list2[j])
+            j += 1
+    while i < len(list1):
+        result.append(list1[i])
+        i += 1
+    while j < len(list2):
+        result.append(list2[j])
+        j += 1
+    return result
+
+list1 = [1, 3, 5, 7]
+list2 = [2, 4, 6, 8]
+merged_list = merge_sorted_lists(list1, list2)
+print("Merged sorted list:", merged_list)
